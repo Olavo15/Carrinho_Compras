47f43a1b5084841eb8012e421c4cf58c02be5dff
Olavo15 <olavoregis169@gmail.com>
2024-07-16 17:53:57 -0300

    fazendo a logica para deletar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import createItem from './service/item.js';
+import * as cartService from './service/cart.js';
 
 const  cart = [];
+const myWhishList = [];
 
 console.log("Welcome to the tour shopee Cart!")
 
-const item1 = await createItem("hotwheels ferrari", 20.99, 1);
-const item2 = await createItem("spider man t-shirt", 15.99, 2);
+const item1 = await createItem("hotwheels ferrari", 20.99, 7);
+const item2 = await createItem("spider man t-shirt", 39.99, 1);
+const item3 = await createItem("SSD 1tb", 152.00 , 1);
 
-console.log(item2.subtotal());
+await cartService.addItem(cart, item1);
+await cartService.addItem(myWhishList, item2);
+await cartService.addItem(myWhishList, item3);
+
+
+
+await cartService.deleteItem(myWhishList, item3.name)
+
+console.log("Shopee cart TOTAL IS:");
+await cartService.calculaleTotal(cart);
+await cartService.calculaleTotal(myWhishList);
--- src/service/cart.js
+++ src/service/cart.js
@@ -2,10 +2,23 @@ async function addItem(UserCart, item){
     UserCart.push(item);
 }
 
-async function deleteItem(UserCart, name){}
+async function deleteItem(UserCart, name){
+    const index = UserCart.findIndex((item) => item.name === name);
+    if(index !== -1){
+        UserCart.splice(index,1);
+    }
+}
 
 async function removeItem(UserCart, index){}
 
 async function calculaleTotal(UserCart){
-    
+    const result = UserCart.reduce((total, item) => total + item.subtotal(), 0);
+    console.log(result.toFixed(2));
+}
+
+export{
+    addItem,
+    deleteItem,
+    removeItem,
+    calculaleTotal,
 }
